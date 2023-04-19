a918d26059ad8de74803e699c93a07f44c4cc4da
Mina1405 <becalvarado@gmail.com>
2023-04-19 11:11:02 -0600

    actualizacion de nombres

--- DAL/MANTENIMIENTOS/cls_Clientes_DAL.cs
+++ DAL/MANTENIMIENTOS/cls_Clientes_DAL.cs
@@ -9,11 +9,11 @@ namespace DAL.MANTENIMIENTOS
 {
     public class cls_Clientes
     {
-        private int _ID_Cliente;
+        private int _ID_CLIENTE;
         private string _NOMBRE_CLIENTE, _APELLIDO1_CLIENTE, _APELLIDO2_CLIENTE, _DIRECCION_CLIENTE, _CORREO_CLIENTE, _sMsjError, _TELEFONO;
         private DataTable _dtDatos, _dtParametros;
 
-        public int ID_Cliente { get => _ID_Cliente; set => _ID_Cliente = value; }
+        public int ID_CLIENTE { get => _ID_CLIENTE; set => _ID_CLIENTE = value; }
         public string TELEFONO { get => _TELEFONO; set => _TELEFONO = value; }
         public string NOMBRE_CLIENTE { get => _NOMBRE_CLIENTE; set => _NOMBRE_CLIENTE = value; }
         public string APELLIDO1_CLIENTE { get => _APELLIDO1_CLIENTE; set => _APELLIDO1_CLIENTE = value; }
