2089fec26f52698b95ccfccb0469b3fbfbcdf9c6
Mina1405 <becalvarado@gmail.com>
2023-04-22 22:40:47 -0600

    Clases: Citas, Doctores y Especialidades

---
+++ BLL/MANTENIMIENTOS/cls_Especialidades_BLL.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.MANTENIMIENTOS;
+using System.Configuration;
+
+namespace BLL.MANTENIMIENTOS
+{
+    class cls_Especialidades_BLL
+    {
+
+        // LISTAR Y FILTRAR
+        public void List_ESPECIALIDADES(ref cls_Especialidades_DAL Obj_Especialidad_DAL)
+        {
+
+            DBConnection.BDClient OBJ_WCF = new DBConnection.BDClient();
+
+
+            if (Obj_Especialidad_DAL.ID_ESPECIALIDAD == 0)
+            {
+                Obj_Especialidad_DAL.DtParametros = null;
+                Obj_Especialidad_DAL.DtDatos = OBJ_WCF.ListarFiltrar("SCH_ADMIN.ESPECIALIDADES", ConfigurationManager.AppSettings["LISTAR_ESPECIALIDADES"], null);
+            }
+            else
+            {
+                Obj_Especialidad_DAL.DtParametros = OBJ_WCF.Get_DT_Param(Obj_Especialidad_DAL.DtParametros);
+
+                Obj_Especialidad_DAL.DtParametros.Rows.Add("@id_especialidad", "1", Obj_Especialidad_DAL.ID_ESPECIALIDAD);
+
+                Obj_Especialidad_DAL.DtDatos = OBJ_WCF.ListarFiltrar("SCH_ADMIN.ESPECIALIDADES", ConfigurationManager.AppSettings["FILTRAR_ESPECIALIDADES"], Obj_Especialidad_DAL.DtParametros);
+            }
+        }
+
+        //GUARDAR Y ACTUALIZAR
+        public void Guardar_ESPECIALIDADES(ref cls_Especialidades_DAL Obj_Especialidad_DAL)
+        {
+            DBConnection.BDClient OBJ_WCF = new DBConnection.BDClient();
+
+
+
+
+
+            Obj_Especialidad_DAL.DtParametros = OBJ_WCF.Get_DT_Param(Obj_Especialidad_DAL.DtParametros);
+
+            Obj_Especialidad_DAL.DtParametros.Rows.Add("@id_especialidad", "6", Obj_Especialidad_DAL.ID_ESPECIALIDAD);
+            Obj_Especialidad_DAL.DtParametros.Rows.Add("@nombre_especialidad", "6", Obj_Especialidad_DAL.NOMBRE_ESPECIALIDAD);
+
+
+
+
+
+
+            //SI LA TABLA NO ES IDENTITY SE ENVIA "NORMAL" DE LO CONTRARIO CUALQUIER OTRO VALOR
+
+            Obj_Especialidad_DAL.SMsjError = OBJ_WCF.Ins_Upd_Delete(ConfigurationManager.AppSettings["GUARDAR_ESPECIALIDADES"], "NORMAL", Obj_Especialidad_DAL.DtParametros);
+        }
+
+        public void Modificar_ESPECIALIDADES(ref cls_Especialidades_DAL Obj_Especialidad_DAL)
+        {
+
+            DBConnection.BDClient OBJ_WCF = new DBConnection.BDClient();
+
+
+
+            Obj_Especialidad_DAL.DtParametros = OBJ_WCF.Get_DT_Param(Obj_Especialidad_DAL.DtParametros);
+
+            Obj_Especialidad_DAL.DtParametros.Rows.Add("@id_especialidad", "6", Obj_Especialidad_DAL.ID_ESPECIALIDAD);
+            Obj_Especialidad_DAL.DtParametros.Rows.Add("@nombre_especialidad", "6", Obj_Especialidad_DAL.NOMBRE_ESPECIALIDAD);
+
+
+
+            Obj_Especialidad_DAL.SMsjError = OBJ_WCF.Ins_Upd_Delete(ConfigurationManager.AppSettings["EDITAR_ESPECIALIDADES"], "NORMAL", Obj_Especialidad_DAL.DtParametros);
+
+
+        }
+
+        //ELIMINAR
+
+        public void Eliminar_ESPECIALIDADES(ref cls_Especialidades_DAL Obj_Especialidad_DAL)
+        {
+            DBConnection.BDClient OBJ_WCF = new DBConnection.BDClient();
+
+            Obj_Especialidad_DAL.DtParametros = OBJ_WCF.Get_DT_Param(Obj_Especialidad_DAL.DtParametros);
+
+            Obj_Especialidad_DAL.DtParametros.Rows.Add("@id_especialidad", "1", Obj_Especialidad_DAL.ID_ESPECIALIDAD);
+
+            Obj_Especialidad_DAL.SMsjError = OBJ_WCF.Ins_Upd_Delete(ConfigurationManager.AppSettings["ELIMINAR_ESPECIALIDADES"], "NORMAL", Obj_Especialidad_DAL.DtParametros);
+        }
+    }
+}
--- PL/Default.aspx.cs
+++ PL/Default.aspx.cs
@@ -13,7 +13,7 @@ namespace PL
     {
         #region VARIABLES GLOBALES 
 
-        cls_Clientes Obj_CLIENTES_DAL = new cls_Clientes();
+        cls_Clientes_DAL Obj_CLIENTES_DAL = new cls_Clientes_DAL();
         cls_Clientes_BLL Obj_CLIENTES_BLL = new cls_Clientes_BLL();
 
         #endregion
@@ -42,18 +42,17 @@ namespace PL
 
         private void CargarDatos()
         {
-            if (txt_filtro.Text.Trim() != "")
-            {
+           if (txt_filtro.Text.Trim() !="") {
                 Obj_CLIENTES_DAL.ID_CLIENTE = int.Parse(txt_filtro.Text.Trim());
             }
-            else
+           else
             {
-
+                
                 Obj_CLIENTES_DAL.ID_CLIENTE = 0;
             }
+            
 
-
-            Obj_CLIENTES_BLL.List_Clientes(ref Obj_CLIENTES_DAL);
+           Obj_CLIENTES_BLL.List_Clientes(ref Obj_CLIENTES_DAL);
 
             dgv_regiones.DataSource = null;
             dgv_regiones.DataSource = Obj_CLIENTES_DAL.DtDatos;
@@ -62,19 +61,19 @@ namespace PL
 
         private void CargarCombos()
         {
-            /* Obj_Regiones_DAL.sTerritoryDescription = "";
+           /* Obj_Regiones_DAL.sTerritoryDescription = "";
 
-             //Obj_Regiones_BLL.List_Filt_REGIONES(ref Obj_Regiones_DAL);
+            //Obj_Regiones_BLL.List_Filt_REGIONES(ref Obj_Regiones_DAL);
 
-             Obj_Regiones_DAL.DtDatos.Rows.Add("0", "--- SELECCIONE UNA REGIÓN ---");
+            Obj_Regiones_DAL.DtDatos.Rows.Add("0", "--- SELECCIONE UNA REGIÓN ---");
 
-             ddl_Regiones.DataSource = null;
-             ddl_Regiones.DataTextField = "NOMBRE REGIÓN";
-             ddl_Regiones.DataValueField = "ID REGIÓN";
-             ddl_Regiones.DataSource = Obj_Regiones_DAL.DtDatos;
-             ddl_Regiones.DataBind();
+            ddl_Regiones.DataSource = null;
+            ddl_Regiones.DataTextField = "NOMBRE REGIÓN";
+            ddl_Regiones.DataValueField = "ID REGIÓN";
+            ddl_Regiones.DataSource = Obj_Regiones_DAL.DtDatos;
+            ddl_Regiones.DataBind();
 
-             ddl_Regiones.SelectedValue = "0";*/
+            ddl_Regiones.SelectedValue = "0";*/
         }
 
 
@@ -89,13 +88,13 @@ namespace PL
             Obj_CLIENTES_DAL.DIRECCION_CLIENTE = txt_direccion.Text.Trim();
             Obj_CLIENTES_DAL.TELEFONO = txt_telefono.Text.Trim();
             Obj_CLIENTES_DAL.CORREO_CLIENTE = txt_correo.Text.Trim();
-
+ 
 
             Obj_CLIENTES_BLL.Guardar_CLIENTES(ref Obj_CLIENTES_DAL);
 
             txt_filtro.Text = string.Empty;
             txt_nombreCliente.Text = string.Empty;
-            txt_apellido1.Text = string.Empty;
+            txt_apellido1.Text =  string.Empty;
             txt_apellido2.Text = string.Empty;
             txt_direccion.Text = string.Empty;
             txt_telefono.Text = string.Empty;
@@ -103,6 +102,7 @@ namespace PL
             CargarDatos();
 
         }
+
 
         protected void txt_Eliminar_Click(object sender, EventArgs e)
         {
@@ -112,6 +112,29 @@ namespace PL
 
             txt_filtro.Text = string.Empty;
             Txt_id.Text = string.Empty;
+            CargarDatos();
+        }
+
+        protected void btn_Modificar_Click(object sender, EventArgs e)
+        {
+            Obj_CLIENTES_DAL.ID_CLIENTE = int.Parse(Txt_id.Text);
+            Obj_CLIENTES_DAL.NOMBRE_CLIENTE = txt_nombreCliente.Text.Trim();
+            Obj_CLIENTES_DAL.APELLIDO1_CLIENTE = txt_apellido1.Text.Trim();
+            Obj_CLIENTES_DAL.APELLIDO2_CLIENTE = txt_apellido2.Text.Trim();
+            Obj_CLIENTES_DAL.DIRECCION_CLIENTE = txt_direccion.Text.Trim();
+            Obj_CLIENTES_DAL.TELEFONO = txt_telefono.Text.Trim();
+            Obj_CLIENTES_DAL.CORREO_CLIENTE = txt_correo.Text.Trim();
+
+
+            Obj_CLIENTES_BLL.Modificar_CLIENTE(ref Obj_CLIENTES_DAL);
+
+            txt_filtro.Text = string.Empty;
+            txt_nombreCliente.Text = string.Empty;
+            txt_apellido1.Text = string.Empty;
+            txt_apellido2.Text = string.Empty;
+            txt_direccion.Text = string.Empty;
+            txt_telefono.Text = string.Empty;
+            txt_correo.Text = string.Empty;
             CargarDatos();
         }
     }
