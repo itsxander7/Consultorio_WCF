a14b04abb9cd15458ab4f6cefc65049330fe826b
Mina1405 <becalvarado@gmail.com>
2023-04-29 02:25:52 -0600

    Actualizacion de la clase Facturacion

--- BLL/MANTENIMIENTOS/cls_Facturacion_BLL.cs
+++ BLL/MANTENIMIENTOS/cls_Facturacion_BLL.cs
@@ -11,6 +11,7 @@ namespace BLL.MANTENIMIENTOS
 {
     public class cls_Facturacion_BLL
     {
+        #region Metodos
         // LISTAR Y FILTRAR
         public void List_Facturas(ref cls_Facturacion_DAL Obj_Facturacion_DAL)
         {
@@ -18,69 +19,10 @@ namespace BLL.MANTENIMIENTOS
             BD_Connection.BDClient OBJ_WCF = new BD_Connection.BDClient();
 
 
-            if (Obj_Facturacion_DAL.uID_Factura == 0)
-            {
-                Obj_Facturacion_DAL.dtParametros = null;
-                Obj_Facturacion_DAL.dtDatos = OBJ_WCF.ListarFiltrar("SCH_ADMIN.FACTURAS", ConfigurationManager.AppSettings["LISTAR_FACTURAS"], null);
-            }
-            else
-            {
-                Obj_Facturacion_DAL.dtParametros = OBJ_WCF.Get_DT_Param(Obj_Facturacion_DAL.dtParametros);
-
-                Obj_Facturacion_DAL.dtParametros.Rows.Add("@ID_FACTURA", "1", Obj_Facturacion_DAL.uID_Factura);
-
-                Obj_Facturacion_DAL.dtDatos = OBJ_WCF.ListarFiltrar("SCH_ADMIN.FACTURAS", ConfigurationManager.AppSettings["FILTRAR_FACTURAS"], Obj_Facturacion_DAL.dtParametros);
-            }
-        }
-
-        //GUARDAR Y ACTUALIZAR
-        public void Guardar_Facturas(ref cls_Facturacion_DAL Obj_Facturacion_DAL)
-        {
-            BD_Connection.BDClient OBJ_WCF = new BD_Connection.BDClient();
-
-
-
-
-
-            Obj_Facturacion_DAL.dtParametros = OBJ_WCF.Get_DT_Param(Obj_Facturacion_DAL.dtParametros);
-
-            Obj_Facturacion_DAL.dtParametros.Rows.Add("@ID_FACTURA", "1", Obj_Facturacion_DAL.uID_Factura);
-            Obj_Facturacion_DAL.dtParametros.Rows.Add("@ID_METODO_PAGO", "1", Obj_Facturacion_DAL.uID_MetodoPago);
-            Obj_Facturacion_DAL.dtParametros.Rows.Add("@ID_CLIENTE", "1", Obj_Facturacion_DAL.uID_Cliente);
-            Obj_Facturacion_DAL.dtParametros.Rows.Add("@TOTAL_NETO", "3", Obj_Facturacion_DAL.fTotalNeto);
-
-            //SI LA TABLA NO ES IDENTITY SE ENVIA "NORMAL" DE LO CONTRARIO CUALQUIER OTRO VALOR
-
-            Obj_Facturacion_DAL.sMsjError = OBJ_WCF.Ins_Upd_Delete(ConfigurationManager.AppSettings["GUARDAR_FACTURAS"], "NORMAL", Obj_Facturacion_DAL.dtParametros);
-        }
-
-        public void Modificar_Facturas(ref cls_Facturacion_DAL Obj_Facturacion_DAL)
-        {
-
-            BD_Connection.BDClient OBJ_WCF = new BD_Connection.BDClient();
-
-            Obj_Facturacion_DAL.dtParametros = OBJ_WCF.Get_DT_Param(Obj_Facturacion_DAL.dtParametros);
-
-            Obj_Facturacion_DAL.dtParametros.Rows.Add("@ID_FACTURA", "1", Obj_Facturacion_DAL.uID_Factura);
-            Obj_Facturacion_DAL.dtParametros.Rows.Add("@ID_METODO_PAGO", "1", Obj_Facturacion_DAL.uID_MetodoPago);
-            Obj_Facturacion_DAL.dtParametros.Rows.Add("@ID_CLIENTE", "1", Obj_Facturacion_DAL.uID_Cliente);
-            Obj_Facturacion_DAL.dtParametros.Rows.Add("@TOTAL_NETO", "3", Obj_Facturacion_DAL.fTotalNeto);
-
-            Obj_Facturacion_DAL.sMsjError = OBJ_WCF.Ins_Upd_Delete(ConfigurationManager.AppSettings["EDITAR_FACTURAS"], "NORMAL", Obj_Facturacion_DAL.dtParametros);
+            Obj_Facturacion_DAL.dtParametros = null;
+            Obj_Facturacion_DAL.dtDatos = OBJ_WCF.ListarFiltrar("SCH_ADMIN.DETALLE_FACTURA", ConfigurationManager.AppSettings["FILTRAR_FACTURAS"], null);
 
         }
-
-        //ELIMINAR
-
-        public void Eliminar_Facturas(ref cls_Facturacion_DAL Obj_Facturacion_DAL)
-        {
-            BD_Connection.BDClient OBJ_WCF = new BD_Connection.BDClient();
-
-            Obj_Facturacion_DAL.dtParametros = OBJ_WCF.Get_DT_Param(Obj_Facturacion_DAL.dtParametros);
-
-            Obj_Facturacion_DAL.dtParametros.Rows.Add("@ID_FACTURA", "1", Obj_Facturacion_DAL.uID_Factura);
-
-            Obj_Facturacion_DAL.sMsjError = OBJ_WCF.Ins_Upd_Delete(ConfigurationManager.AppSettings["ELIMINAR_FACTURAS"], "NORMAL", Obj_Facturacion_DAL.dtParametros);
-        }
+        #endregion
     }
 }
